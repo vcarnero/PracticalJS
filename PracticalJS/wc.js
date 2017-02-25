a68361adf3c08ede4f3ed24469b9a42e3029acec
Veronica Carnero <vcarnero@gmail.com>
2017-02-25 12:22:06 -0700

    readable

--- PracticalJS/wc.js
+++ PracticalJS/wc.js
@@ -1,20 +1,23 @@
 +"# Javascript practice // Code goes here practice
  
-  // js
- // It should have a function to display todos.
+// js
+// It should have a function to display todos.
+// It should have a function to add todos.
+// It should have a function to change todos.
+// It should have a function to delete todos.
  var todoList = {
      todos: ['item 1', 'item 2', 'item 3'],
      displayTodos: function() {
          console.log('My Todos', this.todos);
-     },// It should have a function to add todos.
+     },
      addTodo: function(todo) {
          this.todos.push(todo);
          this.displayTodos();
-     },// It should have a function to change todos.
+     },
      changeTodo: function(position, newValue) {
          this.todos[position] = newValue;
          this.displayTodos();
-     },// It should have a function to delete todos.
+     },
      deleteTodo: funciton(position) {
          this.todos.splice(position, 1);
          this.displayTodos();
