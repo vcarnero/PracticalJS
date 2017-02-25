a72ba5e2cf4bf591d1828ef163f162e02b6f49c0
Veronica Carnero <vcarnero@gmail.com>
2017-02-25 12:23:34 -0700

    v4 requirements

---
+++ PracticalJS/v4boolean.js
@@ -0,0 +1,5 @@
+// V4 Requirements
+//todoList.addTodo should add objects
+//todList.changeTodo should change the todo Text property
+//todoList.toggleCompleted should change the completed property
+
