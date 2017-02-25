2666eabd8994a028477b31ae42ead31ab1134a17
Veronica Carnero <vcarnero@gmail.com>
2017-02-25 13:24:36 -0700

    fix

--- PracticalJS/v4boolean.js
+++ PracticalJS/v4boolean.js
@@ -3,49 +3,24 @@
 //todList.changeTodo should change the todo Text property
 //todoList.toggleCompleted should change the completed property
 //Boolean value
- var todoList = {
-     todos: [],
-     displayTodos: function() {
-         console.log('My Todos', this.todos);
-     },
-     addTodo: function(todoText) {
-         this.todos.push({
-             todoText: todoText,// V4 Requirements
-//todoList.addTodo should add objects
-//todList.changeTodo should change the todo Text property
-//todoList.toggleCompleted should change the completed property
-//Boolean value
- var todoList = {
-     todos: [],
-     displayTodos: function() {
-         console.log('My Todos', this.todos);
-     },
-     addTodo: function(todoText) {
-         this.todos.push({
-             todoText: todoText,
-             completed: false
-         });
-         this.displayTodos();
-     },
-     changeTodo: function(position, newValue) {
-         this.todos[position] = newValue;
-         this.displayTodos();
-     },
-     deleteTodo: funciton(position) {
-         this.todos.splice(position, 1);
-         this.displayTodos();
-     }
-     };
-             completed: false
-         });
-         this.displayTodos();
-     },
-     changeTodo: function(position, newValue) {
-         this.todos[position] = newValue;
-         this.displayTodos();
-     },
-     deleteTodo: funciton(position) {
-         this.todos.splice(position, 1);
-         this.displayTodos();
-     }
-     };
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: funciton(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
